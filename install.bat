7e34182c6435c0a87ec1a1410ced3aae89c79a9c
PhoenixAscencio <nationlouis@gmail.com>
2024-06-17 13:10:30 +0200

    Updated install bat and requirements

--- install.bat
+++ install.bat
@@ -7,10 +7,10 @@ call .venv\Scripts\activate
 pip install -r requirements.txt
 
 
-pip install --upgrade torch==2.2.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install --upgrade torch==2.2.0 torchvision torchaudio xformers==0.0.24 --index-url https://download.pytorch.org/whl/cu118
 
-# use this if you are using CUDA 12.1
-# pip install --upgrade torch==2.2.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+REM use this if you are using CUDA 12.1
+REM pip install --upgrade torch==2.2.0 torchvision torchaudio xformers==0.0.24 --index-url https://download.pytorch.org/whl/cu121
 
 pip install git+https://github.com/openai/CLIP.git
 
